49e5b72b5cd8408ecb8cc57c632c981eb2b2ed32
Neimaad <damienlaoussing@hotmail.com>
2022-12-03 21:53:38 +0100

    refacto code

--- src/main/java/com/cedalanavi/projet_IJVA500_SOA_projets/ProjetIJVA500SoaProjetsApplication.java
+++ src/main/java/com/cedalanavi/projet_IJVA500_SOA_projets/ProjetIJVA500SoaProjetsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetIjva500SoaProjetsApplication {
+public class ProjetIJVA500SoaProjetsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetIjva500SoaProjetsApplication.class, args);
+		SpringApplication.run(ProjetIJVA500SoaProjetsApplication.class, args);
 	}
 
 }
--- src/test/java/com/cedalanavi/projet_IJVA500_SOA_projets/ProjetIJVA500SoaProjetsApplicationTests.java
+++ src/test/java/com/cedalanavi/projet_IJVA500_SOA_projets/ProjetIJVA500SoaProjetsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjetIjva500SoaProjetsApplicationTests {
+class ProjetIJVA500SoaProjetsApplicationTests {
 
 	@Test
 	void contextLoads() {
